5a52ddf1c8c7e158a198b91855814552150cd45b
Chad Thomas <chad.thomas@neudesic.com>
2022-02-19 18:36:29 -0500

    migrate template site to react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar")?.classList.add("open");
+  };
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar")?.classList.remove("open");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <head>
+        <link rel="stylesheet" href="style.css" />
+        <title>Emerald Tiger</title>
+      </head>
+      <body>
+        <div>
+          <header className="header">
+            <div className="brand">
+              <button onClick={openMenu}>&#9776;</button>
+              <a href="index.html">Emerald Tiger</a>
+            </div>
+            <div className="header-links">
+              <a href="cart.html">Cart</a>
+              <a href="signin.html">Sign In</a>
+            </div>
+          </header>
+          <aside className="sidebar">
+            <h3>Shopping Categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>
+              x
+            </button>
+            <ul>
+              <li>
+                <a href="index.html">Pants</a>
+              </li>
+              <li>
+                <a href="index.html">Shirts</a>
+              </li>
+            </ul>
+          </aside>
+          <main className="main">
+            <div className="content">
+              <ul className="products">
+                <li className="product">
+                  <img
+                    className="product-image"
+                    src="images/d1.jpg"
+                    alt="product"
+                  />
+                  <div className="product-name">
+                    <a href="product.html">Slim-fit Shirt</a>
+                  </div>
+                  <div className="product-brand">Nike</div>
+                  <div className="product-price">$60</div>
+                  <div className="product-rating">4.5 Stars (10 reviews)</div>
+                </li>
+                <li className="product">
+                  <img
+                    className="product-image"
+                    src="images/d1.jpg"
+                    alt="product"
+                  />
+                  <div className="product-name">
+                    <a href="product.html">Slim-fit Shirt</a>
+                  </div>
+                  <div className="product-brand">Nike</div>
+                  <div className="product-price">$60</div>
+                  <div className="product-rating">4.5 Stars (10 reviews)</div>
+                </li>
+                <li className="product">
+                  <img
+                    className="product-image"
+                    src="images/d1.jpg"
+                    alt="product"
+                  />
+                  <div className="product-name">
+                    <a href="product.html">Slim-fit Shirt</a>
+                  </div>
+                  <div className="product-brand">Nike</div>
+                  <div className="product-price">$60</div>
+                  <div className="product-rating">4.5 Stars (10 reviews)</div>
+                </li>
+                <li className="product">
+                  <img
+                    className="product-image"
+                    src="images/d1.jpg"
+                    alt="product"
+                  />
+                  <div className="product-name">
+                    <a href="product.html">Slim-fit Shirt</a>
+                  </div>
+                  <div className="product-brand">Nike</div>
+                  <div className="product-price">$60</div>
+                  <div className="product-rating">4.5 Stars (10 reviews)</div>
+                </li>
+                <li className="product">
+                  <img
+                    className="product-image"
+                    src="images/d1.jpg"
+                    alt="product"
+                  />
+                  <div className="product-name">
+                    <a href="product.html">Slim-fit Shirt</a>
+                  </div>
+                  <div className="product-brand">Nike</div>
+                  <div className="product-price">$60</div>
+                  <div className="product-rating">4.5 Stars (10 reviews)</div>
+                </li>
+              </ul>
+            </div>
+          </main>
+          <footer className="footer">&copy 2022 Emerald Tiger</footer>
+        </div>
+      </body>
     </div>
   );
 }
