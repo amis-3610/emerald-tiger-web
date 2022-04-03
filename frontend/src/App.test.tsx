cdb0fd968921bfe3eb9bae56142fef6aa9b632a7
Chad Thomas <chad.thomas@neudesic.com>
2022-04-03 13:45:26 -0400

    adds simple app tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,13 +4,29 @@ import App from './App';
 
 describe('The main app', () => {
 
-  beforeAll(() => {
+  it('should render the Home header', () => {
     render(<App />);
+    // expect(screen.getAllByText('Home')).toBeInTheDocument();
+    expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
   });
 
-  it('should render the Orders', () => {
+  it('should render the Home link', () => {
+    render(<App />);
+    // expect(screen.getAllByText('Home')).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: "Home" })).toBeInTheDocument();
+  });
+
+  it('should render the Orders link', () => {
+    render(<App />);
     expect(screen.getByText('Orders')).toBeInTheDocument();
   });
+
+  it('should render the Payments link', () => {
+    render(<App />);
+    expect(screen.getByText('Payments')).toBeInTheDocument();
+  });
+
+
 
 
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,49 +21,46 @@ function App() {
       <div className="grid-container">
         <link rel="stylesheet" href="style.css" />
         <title>Emerald Tiger</title>
-
-        <body>
-          <div>
-            <header className="header">
-              <div className="brand">
-                <button onClick={openMenu}>&#9776;</button>
-                <a href="index.html">Emerald Tiger</a>
-              </div>
-              <div className="header-links">
-                <Link to="/">Home</Link>
-                <Link to="/catalog">Catalog</Link>
-                <Link to="/orders">Orders</Link>
-                <Link to="/payment">Payments</Link>
-                <Link to="/fullfillment">Fullfillments</Link>
-              </div>
-            </header>
-            <aside className="sidebar">
-              <h3>Shopping Categories</h3>
-              <button className="sidebar-close-button" onClick={closeMenu}>
-                x
-              </button>
-              <ul>
-                <li>
-                  <a href="index.html">Pants</a>
-                </li>
-                <li>
-                  <a href="index.html">Shirts</a>
-                </li>
-              </ul>
-            </aside>
-            <main className="main">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/catalog" element={<Products />} />
-                <Route path="/orders" element={<Orders />}></Route>
-                <Route path="/payment" element={<Payment />}></Route>
-                <Route path="/fullfillment" element={<Fullfillment />}></Route>
-                <Route path="/order/:id" element={<OrderDetail />}></Route>
-              </Routes>
-            </main>
-            <footer className="footer">&copy 2022 Emerald Tiger</footer>
-          </div>
-        </body>
+        <div>
+          <header className="header">
+            <div className="brand">
+              <button onClick={openMenu}>&#9776;</button>
+              <a href="index.html">Emerald Tiger</a>
+            </div>
+            <div className="header-links">
+              <Link to="/">Home</Link>
+              <Link to="/catalog">Catalog</Link>
+              <Link to="/orders">Orders</Link>
+              <Link to="/payment">Payments</Link>
+              <Link to="/fullfillment">Fullfillments</Link>
+            </div>
+          </header>
+          <aside className="sidebar">
+            <h3>Shopping Categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>
+              x
+            </button>
+            <ul>
+              <li>
+                <a href="index.html">Pants</a>
+              </li>
+              <li>
+                <a href="index.html">Shirts</a>
+              </li>
+            </ul>
+          </aside>
+          <main className="main">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/catalog" element={<Products />} />
+              <Route path="/orders" element={<Orders />}></Route>
+              <Route path="/payment" element={<Payment />}></Route>
+              <Route path="/fullfillment" element={<Fullfillment />}></Route>
+              <Route path="/order/:id" element={<OrderDetail />}></Route>
+            </Routes>
+          </main>
+          <footer className="footer">&copy 2022 Emerald Tiger</footer>
+        </div>
       </div>
     </Router>
   );
