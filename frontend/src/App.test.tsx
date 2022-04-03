23d9cfde179ab2cbc4911fc10534f83bca64dca7
Chad Thomas <chad.thomas@neudesic.com>
2022-04-03 09:13:36 -0400

    :alembic: updates app test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('The main app', () => {
+
+  beforeAll(() => {
+    render(<App />);
+  });
+
+  it('should render the Orders', () => {
+    expect(screen.getByText('Orders')).toBeInTheDocument();
+  });
+
+
 });
