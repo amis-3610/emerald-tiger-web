123f09f5397f0d03fcdcaa15c4a4c715c1884be4
Chad Thomas <chad.thomas@neudesic.com>
2022-04-08 18:53:33 -0400

    removes unneeded comment

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -6,7 +6,6 @@ describe('The main app', () => {
 
   it('should render the Home header', () => {
     render(<App />);
-    // expect(screen.getAllByText('Home')).toBeInTheDocument();
     expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
   });
 
@@ -15,18 +14,6 @@ describe('The main app', () => {
     // expect(screen.getAllByText('Home')).toBeInTheDocument();
     expect(screen.getByRole('link', { name: "Home" })).toBeInTheDocument();
   });
-
-  it('should render the Orders link', () => {
-    render(<App />);
-    expect(screen.getByText('Orders')).toBeInTheDocument();
-  });
-
-  it('should render the Payments link', () => {
-    render(<App />);
-    expect(screen.getByText('Payments')).toBeInTheDocument();
-  });
-
-
 
 
 });
